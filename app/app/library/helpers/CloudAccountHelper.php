45779d00ad1c9e41ef66d6eb9e0d7baa1b338652
sudhi <sid4it@gmail.com>
2014-11-04 04:56:29 -0800

    minor verbiage.

--- app/app/library/helpers/CloudAccountHelper.php
+++ app/app/library/helpers/CloudAccountHelper.php
@@ -26,28 +26,35 @@ class CloudAccountHelper
 	
 	public static function findCurrentCost($account)
 	{
-		if(!empty($account) && $account->status == 'Completed')
+		if(!empty($account))
 		{
-			$responseJson = AWSBillingEngine::authenticate(array('username' => Auth::user()->username, 'password' => md5(Auth::user()->engine_key)));
-		 	EngineLog::logIt(array('user_id' => Auth::id(), 'method' => 'authenticate', 'return' => $responseJson));
-		 	$obj = json_decode($responseJson);
-			
-			if(!empty($obj) && $obj->status == 'OK')
-		 	{
-				$response = AWSBillingEngine::GetCurrentCost(array('token' => $obj->token));
-				
-				return StringHelper::isJson($response) ? json_decode($response, true) : array('status' => 'error', 'message' => 'Invalid response from processing engine') ;
-			}
-			else if(!empty($obj) && $obj->status == 'error')
+			if($account->status == 'Completed')
 			{
-				Log::error('Request to Account logs failed :' . $obj2->fail_code . ':' . $obj2->fail_message);
-				Log::error('Log :' . implode(' ', $obj2->job_log));
-            	return array('status' => 'error', 'message' => $obj2->fail_message);
-			}
-			else
-				{
-					return array('status' => 'error', 'message' => 'Backend API is down, please try again later!');
+				$responseJson = AWSBillingEngine::authenticate(array('username' => Auth::user()->username, 'password' => md5(Auth::user()->engine_key)));
+			 	EngineLog::logIt(array('user_id' => Auth::id(), 'method' => 'authenticate', 'return' => $responseJson));
+			 	$obj = json_decode($responseJson);
+				
+				if(!empty($obj) && $obj->status == 'OK')
+			 	{
+					$response = AWSBillingEngine::GetCurrentCost(array('token' => $obj->token));
+					
+					return StringHelper::isJson($response) ? json_decode($response, true) : array('status' => 'error', 'message' => 'Invalid response from processing engine') ;
 				}
+				else if(!empty($obj) && $obj->status == 'error')
+				{
+					Log::error('Request to Account logs failed :' . $obj2->fail_code . ':' . $obj2->fail_message);
+					Log::error('Log :' . implode(' ', $obj2->job_log));
+	            	return array('status' => 'error', 'message' => $obj2->fail_message);
+				}
+				else
+					{
+						return array('status' => 'error', 'message' => 'Backend API is down, please try again later!');
+					}
+			 }
+			 else
+			 	{
+			 		return array('status' => 'error', 'message' => 'Please wait..account in '. $account->status);
+			 	}
 		}
 		else if(empty($account)) 
 		{
