ec58a0e7545ac2134d1cde9000eb6dec5acc580f
sudhi <sid4it@gmail.com>
2014-11-05 10:40:18 -0800

    Example chart.

--- app/app/views/site/dashboard_view.blade.php
+++ app/app/views/site/dashboard_view.blade.php
@@ -27,7 +27,9 @@
 								
 								<h4 class="media-heading">{{ String::title($account->name) }} </h4> <span class="glyphicon glyphicon-calendar"></span> <strong>Created Date</strong>:{{{ $account->created_at }}}
 								<p class="chart">
-									
+									<svg>
+										
+									</svg>
 									
 								</p>
 								
@@ -49,5 +51,40 @@
 <script>
 $( document ).ready(function() {
 	alert('Dashboard loaded');
+	nv.addGraph(function() {
+    var chart = nv.models.multiBarChart()
+      .transitionDuration(350)
+      .reduceXTicks(true)   //If 'false', every single x-axis tick label will be rendered.
+      .rotateLabels(0)      //Angle to rotate x-axis labels.
+      .showControls(true)   //Allow user to switch between 'Grouped' and 'Stacked' mode.
+      .groupSpacing(0.1)    //Distance between each group of bars.
+    ;
+
+    chart.xAxis
+        .tickFormat(d3.format(',f'));
+
+    chart.yAxis
+        .tickFormat(d3.format(',.1f'));
+
+    d3.select('.chart svg')
+        .datum(exampleData())
+        .call(chart);
+
+    nv.utils.windowResize(chart.update);
+
+    return chart;
+});
+
+//Generate some nice data.
+function exampleData() {
+  return stream_layers(3,10+Math.random()*100,.1).map(function(data, i) {
+    return {
+      key: 'Stream #' + i,
+      values: data
+    };
+  });
+}
+	
+	
 });
 </script>
