e3d9e342b68e1ba20f5102ea607b3fc3c3289d6a
sudhi <sid4it@gmail.com>
2014-11-05 07:25:39 -0800

    minor.

--- app/app/library/helpers/UIHelper.php
+++ app/app/library/helpers/UIHelper.php
@@ -233,7 +233,7 @@ Inverse	<span class="label label-inverse">Inverse</span>
 			}
 			$str .= 'Total :' . $data->total .'<br>';
 			$str .= '<span class="glyphicon glyphicon-calendar">Current Month: </span>'.$data->month;
-			$str .= '<span class="glyphicon glyphicon-calendar">Last Updated: </span>'.StringHelper::timeAgo($data->lastUpdate);
+			$str .= '<br/><span class="glyphicon glyphicon-calendar">Last Updated: </span>'.StringHelper::timeAgo($data->lastUpdate);
 		}
 		else if($data->status == 'error')
 		{
