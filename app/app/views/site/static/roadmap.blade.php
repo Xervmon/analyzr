a20a15654c9d5740e8fef50804fa70474d951db7
sudhi <sid4it@gmail.com>
2014-11-08 15:40:05 -0600

    Verbiage.

--- app/app/views/site/static/roadmap.blade.php
+++ app/app/views/site/static/roadmap.blade.php
@@ -24,7 +24,7 @@
 		</p>
 		<div class="text-center">
 	       	 <!-- You can move inline styles to css file or css block. -->
-	       	 <p><h4><font color="3399FF">Comming soon!.</font></h4></p>
+	       	 <p><h4><font color="3399FF">Coming soon - To support more providers</font></h4></p>
 		    <div id="slider1_container" style="position: relative; top: 0px; center: 0px; width: 980px; height: 100px; overflow: hidden; ">
 		
 		        <!-- Loading Screen -->
@@ -40,10 +40,11 @@
 		        <!-- Slides Container -->
 		        <div u="slides" style="cursor: move; position: absolute; center: 0px; top: 0px; width: 980px; height: 100px; overflow: hidden;">
 		            <div><img u="image" title="Amazon AWS" alt="Amazon AWS " src="{{ asset('/assets/img/providers/'.Config::get('provider_meta.Amazon AWS'.'.logo')) }}" /></div>
-		            <div><img u="image" title="Amazon AWS" alt="Amazon AWS " src="{{ asset('/assets/img/providers/gce.png') }}" /></div>
-		            <div><img u="image" title="Amazon AWS" alt="Amazon AWS " src="{{ asset('/assets/img/providers/openstack_logo.png') }}" /></div>
-		            <div><img u="image" title="Amazon AWS" alt="Amazon AWS " src="{{ asset('/assets/img/providers/rackspace_logo.png') }}" /></div>
-		            <div><img u="image" title="Amazon AWS" alt="Amazon AWS " src="{{ asset('/assets/img/providers/vcloud.png') }}" /></div>
+		            <div><img u="image" title="Microsoft Azure" alt="Microsoft Azure" src="{{ asset('/assets/img/providers/azure.png') }}" /></div>
+		            <div><img u="image" title="GCE" alt="GCE" src="{{ asset('/assets/img/providers/gce.png') }}" /></div>
+		            <div><img u="image" title="Openstack" alt="Openstack" src="{{ asset('/assets/img/providers/openstack_logo.png') }}" /></div>
+		            <div><img u="image" title="Rackspace" alt="Rackspace" src="{{ asset('/assets/img/providers/rackspace_logo.png') }}" /></div>
+		            <div><img u="image" title="VM Ware VCloud" alt="VM Ware VCloud" src="{{ asset('/assets/img/providers/vcloud.png') }}" /></div>
 		        </div>
 	     	</div>
        </div>
