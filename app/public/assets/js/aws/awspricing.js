212ba84b4e670165e1f5bf971191f20923961acb
sudhi <sid4it@gmail.com>
2014-11-07 21:00:18 -0600

    jquery fix

--- app/public/assets/js/aws/awspricing.js
+++ app/public/assets/js/aws/awspricing.js
@@ -5,31 +5,34 @@
     function flattenData(data) {
         console.log('flattenData', data);
         var flatData = [];
-        for (var i = 0, l = data.regions.length; i < l; i++) {
-            var region = data.regions[i];
-            // console.log("Region", region.region);
-            for (var j = 0, k = region.instanceTypes.length; j < k; j++) {
-                var instance = region.instanceTypes[j],
-                    thisData = {
-                        "Region": region.region,
-                        "OS": instance.os,
-                        "Type": instance.type
-                    };
-                if ($.isPlainObject(instance.prices)) {
-                    // Reserved
-                    thisData["Utilization"] = instance.utilization;
-                    thisData['1 Year Hourly (' + data.config.currency + ')'] = instance.prices['1year'].hourly;
-                    thisData['1 Year Upfront (' + data.config.currency + ')'] = instance.prices['1year'].upfront;
-                    thisData['3 Years Hourly (' + data.config.currency + ')'] = instance.prices['3year'].hourly;
-                    thisData['3 Years Upfront (' + data.config.currency + ')'] = instance.prices['3year'].upfront;
-                } else {
-                    // On Demand
-                    thisData['Price (' + data.config.currency + ')'] = instance.price;
-                }
-                // console.log("instanceData", thisData);
-                flatData.push(thisData);
-            }
-        }
+        if(data)
+        {
+	        for (var i = 0, l = data.regions.length; i < l; i++) {
+	            var region = data.regions[i];
+	            // console.log("Region", region.region);
+	            for (var j = 0, k = region.instanceTypes.length; j < k; j++) {
+	                var instance = region.instanceTypes[j],
+	                    thisData = {
+	                        "Region": region.region,
+	                        "OS": instance.os,
+	                        "Type": instance.type
+	                    };
+	                if ($.isPlainObject(instance.prices)) {
+	                    // Reserved
+	                    thisData["Utilization"] = instance.utilization;
+	                    thisData['1 Year Hourly (' + data.config.currency + ')'] = instance.prices['1year'].hourly;
+	                    thisData['1 Year Upfront (' + data.config.currency + ')'] = instance.prices['1year'].upfront;
+	                    thisData['3 Years Hourly (' + data.config.currency + ')'] = instance.prices['3year'].hourly;
+	                    thisData['3 Years Upfront (' + data.config.currency + ')'] = instance.prices['3year'].upfront;
+	                } else {
+	                    // On Demand
+	                    thisData['Price (' + data.config.currency + ')'] = instance.price;
+	                }
+	                // console.log("instanceData", thisData);
+	                flatData.push(thisData);
+	            }
+	        }
+	    }
         console.log("flattened data", flatData);
         return flatData;
     }
