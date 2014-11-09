5374700759a79f999be56f196945c2ec1c01428e
sudhi <sid4it@gmail.com>
2014-11-08 18:09:54 -0600

    Pie chart.

--- app/app/views/site/dashboard_view.blade.php
+++ app/app/views/site/dashboard_view.blade.php
@@ -60,41 +60,76 @@
 
 <script>
 var accountData = '{{ json_encode(CloudAccountHelper::findCurrentCost($account)) }}';
-$( document ).ready(function() {
-	alert('Dashboard loaded');
-	nv.addGraph(function() {
-    var chart = nv.models.multiBarChart()
-      .transitionDuration(350)
-      .reduceXTicks(true)   //If 'false', every single x-axis tick label will be rendered.
-      .rotateLabels(0)      //Angle to rotate x-axis labels.
-      .showControls(true)   //Allow user to switch between 'Grouped' and 'Stacked' mode.
-      .groupSpacing(0.1)    //Distance between each group of bars.
-    ;
+nv.addGraph(function() {
+  var chart = nv.models.pieChart()
+      .x(function(d) { return d.label })
+      .y(function(d) { return d.value })
+      .showLabels(true);
 
-    chart.xAxis
-        .tickFormat(d3.format(',f'));
-
-    chart.yAxis
-        .tickFormat(d3.format(',.1f'));
-	var data = exampleData();
-	console.log(data);
-    d3.select('.chart svg')
+    d3.select("#chart svg")
         .datum(exampleData())
+        .transition().duration(350)
         .call(chart);
 
-    nv.utils.windowResize(chart.update);
-
-    return chart;
+  return chart;
 });
 
-//Generate some nice data.
+//Donut chart example
+nv.addGraph(function() {
+  var chart = nv.models.pieChart()
+      .x(function(d) { return d.label })
+      .y(function(d) { return d.value })
+      .showLabels(true)     //Display pie labels
+      .labelThreshold(.05)  //Configure the minimum slice size for labels to show up
+      .labelType("percent") //Configure what type of data to show in the label. Can be "key", "value" or "percent"
+      .donut(true)          //Turn on Donut mode. Makes pie chart look tasty!
+      .donutRatio(0.35)     //Configure how big you want the donut hole size to be.
+      ;
+
+    d3.select("#chart2 svg")
+        .datum(exampleData())
+        .transition().duration(350)
+        .call(chart);
+
+  return chart;
+});
+
+//Pie chart example data. Note how there is only a single array of key-value pairs.
 function exampleData() {
-  return stream_layers(3,10+Math.random()*100,.1).map(function(data, i) {
-    return {
-      key: 'Stream #' + i,
-      values: data
-    };
-  });
+  return  [
+      { 
+        "label": "One",
+        "value" : 29.765957771107
+      } , 
+      { 
+        "label": "Two",
+        "value" : 0
+      } , 
+      { 
+        "label": "Three",
+        "value" : 32.807804682612
+      } , 
+      { 
+        "label": "Four",
+        "value" : 196.45946739256
+      } , 
+      { 
+        "label": "Five",
+        "value" : 0.19434030906893
+      } , 
+      { 
+        "label": "Six",
+        "value" : 98.079782601442
+      } , 
+      { 
+        "label": "Seven",
+        "value" : 13.925743130903
+      } , 
+      { 
+        "label": "Eight",
+        "value" : 5.1387322875705
+      }
+    ];
 }
 	
 	
