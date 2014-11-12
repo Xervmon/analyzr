ba6e3fedaf3b35e96536ba1f436d5938df621bc2
sudhi <sid4it@gmail.com>
2014-11-12 08:14:33 -0800

    Security accounts

--- app/app/views/site/security_account/index.blade.php
+++ app/app/views/site/security_account/index.blade.php
@@ -6,7 +6,7 @@
 <div class="page-header">
 	<div class="row">
 		<div class="col-md-9">
-			<h5>{{{ Lang::get('account/account.your_accounts') }}}</h5>
+			<h5>{{{ Lang::get('security_account/security_account.your_accounts') }}}</h5>
 		</div>
 	</div>
 </div>
@@ -20,21 +20,21 @@
 						<span class="pull-left" href="#">
 						    <img class="media-object img-responsive" src="{{ asset('/assets/img/providers/'.Config::get('provider_meta.'.$account->cloudProvider.'.logo')) }}" alt="{{ $account->cloudProvider }}" />
 						</span>
-						@if($account->status == Lang::get('account/account.STATUS_IN_PROCESS'))
-							<form class="pull-right" method="post" action="{{ URL::to('account/' . $account->id . '/refresh') }}">
+						@if($account->status == Lang::get('security_account/security_account.STATUS_IN_PROCESS'))
+							<form class="pull-right" method="post" action="{{ URL::to('security_account/' . $account->id . '/refresh') }}">
 									<!-- CSRF Token -->
 									<input type="hidden" name="_token" value="{{{ csrf_token() }}}" />
 									<!-- ./ csrf token -->
 									<button type="submit" class="btn btn-success pull-right" role="button"><span class="glyphicon glyphicon-refresh"></span></button>
 							</form>
 						@endif	
-						<form class="pull-right" method="post" action="{{ URL::to('account/' . $account->id . '/delete') }}">
+						<form class="pull-right" method="post" action="{{ URL::to('security_account/' . $account->id . '/delete') }}">
 							<!-- CSRF Token -->
 							<input type="hidden" name="_token" value="{{{ csrf_token() }}}" />
 							<!-- ./ csrf token -->
 							<button type="submit" class="btn btn-warning pull-right" role="button"><span class="glyphicon glyphicon-trash"></span></button>
 						</form>
-						<a href="{{ URL::to('account/' . $account->id . '/edit') }}" class="btn btn-success pull-right" role="button"><span class="glyphicon glyphicon-edit"></span></a>
+						<a href="{{ URL::to('security_account/' . $account->id . '/edit') }}" class="btn btn-success pull-right" role="button"><span class="glyphicon glyphicon-edit"></span></a>
 						<div class="media-body">
 							<h4 class="media-heading">{{ String::title($account->name) }}</h4>
 							<p>
@@ -46,9 +46,7 @@
 								<a href="{{ URL::to('account/' . $account->id . '/SecurityGroups') }}"><span class="glyphicon glyphicon-lock"></span></a>
 								
 							</p>
-							<p>
-								{{UIHelper::displayCurrentCost($account->id, CloudAccountHelper::findCurrentCost($account))}}
-							</p>
+						
 						</div>
 					</div>
 				</li>
@@ -56,11 +54,11 @@
 		@endif
 	</ul>
 	@if(empty($accounts) || count($accounts) === 0) 
-		<div class="alert alert-info"> {{{ Lang::get('account/account.empty_accounts') }}}</div>
+		<div class="alert alert-info"> {{{ Lang::get('security_account/security_account.empty_accounts') }}}</div>
 	@endif
 </div>
 <div>
-<a href="{{ URL::to('account/create') }}" class="btn btn-primary pull-right" role="button">{{{ Lang::get('account/account.add_account') }}}</a>
+<a href="{{ URL::to('security_account/create') }}" class="btn btn-primary pull-right" role="button">{{{ Lang::get('security_account/security_account.add_account') }}}</a>
 </div>
 
 @stop
