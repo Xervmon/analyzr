566d6e7c429a2ce49f8fcc116b8dcd3f90d323e1
sudhi <sid4it@gmail.com>
2014-11-14 15:29:59 -0800

    Class name change.

--- app/app/database/migrations/2014_11_14_043_add_to_accounts2_table.php
+++ app/app/database/migrations/2014_11_14_043_add_to_accounts2_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class AddToAccountsTable2 extends Migration {
+class AddToAccounts2Table extends Migration {
 
 	/**
 	 * Run the migrations.
