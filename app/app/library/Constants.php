dc1c33d999afa4faafbf1d78ccea4b858ac3c4bf
sudhi <sid4it@gmail.com>
2014-11-14 15:27:49 -0800

    cleanup

--- app/app/library/Constants.php
+++ app/app/library/Constants.php
@@ -15,10 +15,9 @@
 class Constants 
 {
 	const RACKSPACE_CLOUD 					= 'Rackspace Cloud';
-	const AWS_CLOUD			 			    = 'Amazon AWS';
+	
  	const HP_CLOUD 			  				= 'HP Cloud';
- 	const HP_CSM 			  				= 'HP CloudSystem';
-	const OPENSTACK							= 'OpenStack';
+ 	const OPENSTACK							= 'OpenStack';
 	const DIGITAL_OCEAN						= 'DigitalOcean';
 	
 	const BILLING_PROFILE					= 'Billing Profile';
