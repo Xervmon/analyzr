5a0e5227f95c4a1246ac9a11789aa35a68119873
sudhi <sid4it@gmail.com>
2014-11-13 17:22:08 -0800

    Account profile types introduced.

--- app/app/config/dev/account_schema.php
+++ app/app/config/dev/account_schema.php
@@ -7,7 +7,7 @@
 // Schema for the CloudAccount specific fields, will be converted into JSON and used on the front-end with https://github.com/joshfire/jsonform
 
 return array(
-    'Amazon AWS' => array(
+    'Amazon AWS:Billing Profile' => array(
         'credentials[accountId]' => array(
             'type' => 'string',
             'title' => 'Account ID',
@@ -26,6 +26,28 @@ return array(
         'credentials[billingBucket]' => array(
             'type' => 'string',
             'title' => 'Billing Bucket',
+            'required' => true
+        ) ,
+    ) ,
+     'Amazon AWS:Security Profile' => array(
+        'credentials[apiKey]' => array(
+            'type' => 'string',
+            'title' => 'API Key',
+            'required' => true
+        ) ,
+        'credentials[secretKey]' => array(
+            'type' => 'string',
+            'title' => 'Secret Key',
+            'required' => true
+        ) ,
+        'credentials[assumedRole]' => array(
+            'type' => 'string',
+            'title' => 'Assumed Role',
+            'required' => true
+        ) ,
+        'credentials[securityToken]' => array(
+            'type' => 'string',
+            'title' => 'SecurityToken',
             'required' => true
         ) ,
     ) ,
