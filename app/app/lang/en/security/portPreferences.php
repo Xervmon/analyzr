bdb41e9e1458af8a6e920eda12ea64ccb1e61dd1
sudhi <sid4it@gmail.com>
2014-11-15 16:15:35 -0600

    verbiage.

--- app/app/lang/en/security/portPreferences.php
+++ app/app/lang/en/security/portPreferences.php
@@ -10,7 +10,7 @@ return array(
 	|
 	*/
 
-	'preferences'            => 'Preferences',
+	'preferences'            => 'Your Port Preferences',
 	'portPreference_updated' => 'Port Preferences updated',
 	'empty_no_preferences' => 'You do not have any port preferences. Create one by clicking on "Add Port Preference" below',
 	'portPreference_add' => 'Add Port preference'
