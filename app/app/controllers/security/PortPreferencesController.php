0657a30de01faebf99435b5c227b0b1d8954a735
sudhi <sid4it@gmail.com>
2014-11-20 16:38:53 -0600

    project.

--- app/app/controllers/security/PortPreferencesController.php
+++ app/app/controllers/security/PortPreferencesController.php
@@ -44,7 +44,7 @@ class PortPreferencesController extends BaseController {
         $portPreferences = $this->portPreference
         						-> where('portPreferences.user_id', Auth::id())
         						-> join('cloudAccounts', 'cloudAccounts.id', '=', 'portPreferences.cloudAccountId')
-								-> select('portPreferences.id', 'portPreferences.name', 'portPreferences.status', 'cloudAccounts.name as "AccountName"', 'cloudAccounts.cloudProvider', 'cloudAccounts.profileType')
+								-> select('portPreferences.id', 'portPreferences.project', 'portPreferences.status', 'cloudAccounts.name as "AccountName"', 'cloudAccounts.cloudProvider', 'cloudAccounts.profileType')
         						-> orderBy('portPreferences.created_at', 'DESC')
         						-> paginate(10);
 		
