6358f9f5cfbb4f5a1eee0bd4efc892962d89b318
sudhi <sid4it@gmail.com>
2014-11-22 12:33:44 -0600

    minor

--- app/app/controllers/security/SecurityReportsController.php
+++ app/app/controllers/security/SecurityReportsController.php
@@ -56,7 +56,7 @@ class SecurityReportsController extends BaseController {
 			}
 			else 
 			{
-				return View::make('site/security/auditReports', array('table' =>$table ));	
+				return View::make('site/security/reports', array('table' =>$table ));	
 			}
 			
 		}
