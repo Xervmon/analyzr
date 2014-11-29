8802917e0a93b8068a50f79044221ace64905c51
sudhi <sid4it@gmail.com>
2014-11-29 16:07:59 -0600

    minor

--- app/app/library/helpers/CloudAccountHelper.php
+++ app/app/library/helpers/CloudAccountHelper.php
@@ -74,7 +74,11 @@ class CloudAccountHelper
 														  	
 														foreach($costData as $key => $value)
 														{
-															$drilldownSeries ->data = array(0 => $key, 1 => $value);
+															$obj = new stdClass();
+															$obj-> {'0'} = $key;
+															$obj-> {'1'} = $value;
+															$drilldownSeries ->data[] = $obj;
+															//array(0 => $key, 1 => $value);
 														}													
 													}
 				//$arr[][Constants::READONLY_PROFILE] = array($account->name =>self::findCurrentCost($account)); break;
