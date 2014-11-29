32551b6e06b7297b40c278cd9c54cedb90b4f12e
sudhi <sid4it@gmail.com>
2014-11-28 18:23:25 -0600

    Delete the jobs and then delete the account.

--- app/app/controllers/account/AccountController.php
+++ app/app/controllers/account/AccountController.php
@@ -429,8 +429,13 @@ class AccountController extends BaseController {
      */
     public function postDelete($id) {
     		
+    	//Delete the jobs for the account
+    	Log:info('Deleting the jobs for ' . $id .' for ' . Auth::user()->username.' from Process Job');	
+    	ProcessJob::where('user_id', Auth::id())->where('cloudAccountId', $id)->delete();
+    		
     	CloudAccount::where('id', $id)->where('user_id', Auth::id())->delete();
-        
+        Log:info('Deleting the cloud account for ' . $id .' for ' . Auth::user()->username);	
+		
         // Was the comment post deleted?
         $account = CloudAccount::where('user_id', Auth::id())->find($id);
         if (empty($account)) {
