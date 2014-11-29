fb3908cd8048b957c2eea293642bdcceda945c0b
karthikhooduku <kchakravarthi@hooduku.com>
2014-11-29 18:42:20 +0530

    Add account Button moved to top, Error fixed in HomeController, Href added to info pages

--- app/app/views/site/account/assets/awsInfo.blade.php
+++ app/app/views/site/account/assets/awsInfo.blade.php
@@ -6,7 +6,7 @@
 <div class="page-header">
 	<div class="row">
 		<div class="col-md-9">
-			<h5>{{ $account->name }} : {{{ Lang::get('account/account.awsDetails') }}}</h5>
+			<h5><a href="{{ URL::to('account/'.$account->id.'/edit') }}">{{ $account->name }} </a> : {{{ Lang::get('account/account.awsInstanceDetails') }}}</h5>
 		</div>
 	</div>
 </div>
