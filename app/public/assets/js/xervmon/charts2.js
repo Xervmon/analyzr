172037a2e2a0f89e0c18cf63b8d8915e557470f7
karthikhooduku <kchakravarthi@hooduku.com>
2014-11-30 16:08:53 +0530

    Dashboard High-chats fixes

--- app/public/assets/js/xervmon/charts2.js
+++ app/public/assets/js/xervmon/charts2.js
@@ -1,5 +1,72 @@
 ;columnDrilldown = function (selector, columnType, data)
 {
+
+	var brandsData = [];
+               
+	
+
+	            $.each(data.series, function (name, y) {
+                brandsData.push({
+                    name: name,
+                    y: y,
+                    drilldown: name //versions[name] ? name : null
+                });
+            });
+ 
+	$(selector).highcharts({
+                chart: {
+                    type: columnType
+                },
+                credits: {
+			 		enabled : false
+				},
+                title: {
+                    text: data.titleText
+                },
+                subtitle: {
+                    text: data.subtitleText
+                },
+                xAxis: {
+                    type: 'category'
+                },
+                yAxis: {
+                    title: {
+                        text: data.yAxisTitle
+                    }
+                },
+                legend: {
+                    enabled: false
+                },
+                plotOptions: {
+                    series: {
+                        borderWidth: 0,
+                        dataLabels: {
+                            enabled: true,
+                            format: '{point.y:.1f}'
+                        }
+                    }
+                },
+
+                tooltip: {
+                    headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+                    pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}</b> of total<br/>'
+                },
+
+                series: [{
+                    name: data.account,
+                    colorByPoint: true,
+                    data: brandsData
+                }],
+                drilldown: {
+                    series: data.drilldownSeries
+                }
+            });
+};
+
+
+
+/*;columnDrilldown = function (selector, columnType, data)
+{
 	 $('.chart1').highcharts({
 	        chart: {
 	            type: columnType
@@ -42,4 +109,4 @@
 	            }]
 	        }
 	    });
-};
+};*/
