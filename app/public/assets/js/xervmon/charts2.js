d290d2ccd2eec8d77443ce97f0af064b48058400
sudhi <sid4it@gmail.com>
2014-11-29 23:07:35 -0600

    minor

--- app/public/assets/js/xervmon/charts2.js
+++ app/public/assets/js/xervmon/charts2.js
@@ -1,54 +1,45 @@
 ;columnDrilldown = function (selector, columnType, data)
 {
-	//hs.showCredits = false;
-	console.log(data.series);
-	console.log(data.drilldownSeries);
-	$(selector).highcharts({
-                chart: {
-                    type: columnType
-                },
-                credits: {
-			 		enabled : false
-				},
-                title: {
-                    text: data.titleText
-                },
-                subtitle: {
-                    text: data.subtitleText
-                },
-                xAxis: {
-                    type: 'category'
-                },
-                yAxis: {
-                    title: {
-                        text: data.yAxisTitle
-                    }
-                },
-                legend: {
-                    enabled: false
-                },
-                plotOptions: {
-                    series: {
-                        borderWidth: 0,
-                        dataLabels: {
-                            enabled: true,
-                            format: '{point.y:.1f}%'
-                        }
-                    }
-                },
+	 $('.chart1').highcharts({
+	        chart: {
+	            type: columnType
+	        },
+	        title: {
+	            text: data.titleText
+	        },
+	        xAxis: {
+	            type: 'category'
+	        },
 
-                tooltip: {
-                    headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
-                    pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
-                },
+	        legend: {
+	            enabled: false
+	        },
 
-                series: [{
-                    name: 'Accounts',
-                    colorByPoint: true,
-                    data: data.series
-                }],
-                drilldown: {
-                    series: data.drilldownSeries
-                }
-            });
+	        plotOptions: {
+	            series: {
+	                borderWidth: 0,
+	                dataLabels: {
+	                    enabled: true
+	                }
+	            }
+	        },
+
+	        series: [{
+	            name: 'Things',
+	            colorByPoint: true,
+	            data: [{
+	                name: 'Amazon Account',
+	                y: json.series["Amazon Account-ReadOnly Profile"],
+	                drilldown: 'Amazon Account-ReadOnly Profile'
+	            }]
+	        }],
+	        drilldown: {
+	            series: [{
+	                id: json.drilldownSeries[0].id,
+	                data: json.drilldownSeries[0].data
+						 
+						
+	            }]
+	        }
+	    });
 };
