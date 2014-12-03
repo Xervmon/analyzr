531df657194d4c6689919706d8709fe520b2280d
karthikhooduku <kchakravarthi@hooduku.com>
2014-12-03 20:15:28 +0530

    Minor fixes on AWS info

--- app/app/views/site/account/assets/securityGroups.blade.php
+++ app/app/views/site/account/assets/securityGroups.blade.php
@@ -6,7 +6,7 @@
 <div class="page-header">
 	<div class="row">
 		<div class="col-md-9">
-			<h5>{{ $account->name }} : {{{ Lang::get('account/account.securityGroups') }}}</h5>
+			<h5>{{{ Lang::get('account/account.securityGroups') }}} : <a href="{{ URL::to('account/'.$account->id.'/edit') }}">{{ $account->name }} </a>  </h5>
 		</div>
 	</div>
 </div>
