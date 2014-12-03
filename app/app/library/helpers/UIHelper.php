13a68e7d78a5906e5123c0dfa98bb10b16bde110
sudhi <sid4it@gmail.com>
2014-12-03 07:20:42 -0600

    cleaned

--- app/app/library/helpers/UIHelper.php
+++ app/app/library/helpers/UIHelper.php
@@ -385,19 +385,26 @@ Inverse	<span class="label label-inverse">Inverse</span>
 	public static function getPortPreferenceServicesStatus($portPreference)
 	{
 		$obj = json_decode($portPreference->toJson());
-		$processJobs = ProcessJob::where('user_id', Auth::id())->where('cloudAccountId', $obj->cloudAccountId) ->where('operation', Lang::get('account/account.create_secgroup')) -> orderBy('created_at', 'desc') -> first();
+		$processJobs = ProcessJob::where('user_id', Auth::id())->where('cloudAccountId', $obj->cloudAccountId) ->where('operation', Lang::get('account/account.create_secgroup')) -> orderBy('created_at', 'desc') -> get();
 		$processJobs = json_decode($processJobs->toJson());
 		
 		$jobs = empty($account->jobs) ? '' : $account->jobs;
 		$str = '';	
+		$new_prob = array();
 		if(!empty($processJobs))
 		{
+			foreach ($processJobs as $array) {
+				if (!array_key_exists($array->operation, $new_prob)) {
+					$new_prob[$array->operation] = $array;
+				}
+			}
+			$processJobs = $new_prob;
 			$str = '<div class="table-responsive">  <table class="table table-bordered">';
 
 			foreach($processJobs as $job)
 			{
-				$temp_url = URL::full().'/'.$job->cloudAccountId.'/refresh';
-				$temp_url = str_ireplace(Lang::get('security/portPreferences.portPreference'),Lang::get('account/account.account'),$temp_url);
+				$temp_url = URL::to('account').'/'.$obj->cloudAccountId.'/refresh';
+				//$temp_url = str_ireplace(Lang::get('security/portPreferences.portPreference'),Lang::get('account/account.account'),$temp_url);
 				
 				$str .= '<tr>';
 				$str .= '<td> <i class="fa fa-lock"></i> ' . $job -> operation. '</td>';
