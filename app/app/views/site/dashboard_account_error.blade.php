99a616c2fb38fc042c48350f8a716b1f070a9468
sudhi <sid4it@gmail.com>
2014-12-07 10:18:51 -0600

    minor

---
+++ app/app/views/site/dashboard_account_error.blade.php
@@ -0,0 +1,11 @@
+@foreach($chartDataForAccounts['series'] as $row => $value)
+	@if($value == 0)
+		<div class="col-md12">
+			<div class="alert alert-danger alert-block">
+				<button type="button" class="close" data-dismiss="alert">&times;</button>
+				<h4>Error</h4>
+   				<?php echo $row . ' shows 0 total. Please double check the Account Id and credentials for the account ';?>
+			</div>
+		</div>
+	@endif	
+@endforach
--- app/app/views/site/dashboard_view.blade.php
+++ app/app/views/site/dashboard_view.blade.php
@@ -1,16 +1,5 @@
 <div class="page-header">
 	<div class="row">
-		@foreach($chartDataForAccounts['series'] as $row => $value)
-			@if($value == 0)
-				<div class="col-md12">
-					<div class="alert alert-danger alert-block">
-						<button type="button" class="close" data-dismiss="alert">&times;</button>
-						<h4>Error</h4>
-   						<?php echo $row . ' shows 0 total. Please double check the Account Id and credentials for the account ';?>
-					</div>
-				</div>
-			@endif	
-		@endforach
 		<div class="col-md-9">
 			<h5>Dashboard</h5>
 		</div>
