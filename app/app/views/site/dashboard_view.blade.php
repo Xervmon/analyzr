53c4678d6fb8c9f520b37de9bccf1c65222092ef
sudhi <sid4it@gmail.com>
2014-12-07 10:11:14 -0600

    minor

--- app/app/views/site/dashboard_view.blade.php
+++ app/app/views/site/dashboard_view.blade.php
@@ -6,7 +6,7 @@
 					<div class="alert alert-danger alert-block">
 						<button type="button" class="close" data-dismiss="alert">&times;</button>
 						<h4>Error</h4>
-   						$row . ' shows 0 total. Please double check the Account Id and credentials for the account ';
+   						<?php echo $row . ' shows 0 total. Please double check the Account Id and credentials for the account ';?>
 					</div>
 				</div>
 			@endif	
