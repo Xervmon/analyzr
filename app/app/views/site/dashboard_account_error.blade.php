6aade5f9e653144ba608b48b40bde64e5c9bfb44
sudhi <sid4it@gmail.com>
2014-12-07 10:58:24 -0600

    verbiage.

--- app/app/views/site/dashboard_account_error.blade.php
+++ app/app/views/site/dashboard_account_error.blade.php
@@ -4,7 +4,7 @@
 			<div class="alert alert-danger alert-block">
 				<button type="button" class="close" data-dismiss="alert">&times;</button>
 				<h4>Error</h4>
-   				{{ UIHelper::getAccountAnchor($row) }}  {{{ Lang::get('account/account.check_accountId') }}} or {{{ Lang::get('account/account.create_ticket') }}}
+   				{{ UIHelper::getAccountAnchor($row) }}  {{{ Lang::get('account/account.check_accountId') }}} or {{ Lang::get('account/account.create_ticket') }}
 			</div>
 		</div>
 	@endif	
