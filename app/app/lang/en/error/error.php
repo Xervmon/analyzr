87044f756a4f1f79a8c934aedee2d7c044c7f9da
chaitra padaki <padakichaitra@gmail.com>
2014-12-12 18:54:10 +0530

    Amazon logo has been changed, breadcrumb added to error pages

--- app/app/lang/en/error/error.php
+++ app/app/lang/en/error/error.php
@@ -13,5 +13,6 @@ return array(
  'Ohh' => 'Ohh.... ',
  '404_message' => 'Requested Page Not Found',
  '500_message' => 'Looks like something went Wrong',
+ '403_message' => 'Something went wrong..!!',
 
 );
