c84af43dd3ba8fa841ce3cc25ac41ba35b67f9a8
vasanth-hooduku <vasanth@hooduku.com>
2014-12-17 20:38:34 +0530

    budget UI

---
+++ app/app/lang/en/budget/budget.php
@@ -0,0 +1,27 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| User Language Lines
+	|--------------------------------------------------------------------------
+	|
+	|
+	 */
+
+	'budget_updated' => 'Budget updated.',
+	'budget_created' => 'Budget created.',
+	'add_budget' => 'Add Budget',
+	'your_budgets' => 'Your Budgets:',
+	'empty_budgets' => 'You do not have any Budget. Create one by clicking on "Add Budget"',
+	'budget_auth_failed' => 'Budget authentication failed. ',
+	'Budget' => 'Budget:',
+	'choose_an_account' => 'Choose An Account',
+	'setbudget' => 'Set Budget',
+	'budgettype' => 'Budget Type',
+	'notification_email' => 'Notification Email',
+	'budget_delete' => 'Are You Sure Want To Delete Budget For This Account ?',
+
+
+);
--- app/app/views/site/layouts/default.blade.php
+++ app/app/views/site/layouts/default.blade.php
@@ -104,6 +104,8 @@
 	    								<li class="divider"></li>
 	    								<li><a href="{{{ URL::to('enginelog') }}}"><span class="glyphicon glyphicon-inbox"></span> {{{ Lang::get('site.enginelog') }}}</a></li>
 	    								<li class="divider"></li>
+	    								<li><a href="{{{ URL::to('budget') }}}"><i class="fa fa-money"></i> {{{ Lang::get('site.budget') }}}</a></li>
+	    								<li class="divider"></li>
 	    								<li><a href="{{{ URL::to('ServiceStatus') }}}"><span class="glyphicon glyphicon-signal"></span> {{{ Lang::get('site.webservice_status') }}}</a></li>
 	    								
 	    							</ul>
