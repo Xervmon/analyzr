fd547e8b3c1aed6c7c532270239113c40ba54c7f
sudhi <sid4it@gmail.com>
2015-01-14 23:14:23 -0600

    minor

--- app/app/lang/en/home.php
+++ app/app/lang/en/home.php
@@ -21,6 +21,6 @@ return array(
             'description' => 'We aggregate your spend and analyze your cost data!'
         )
     ),
-    'xervmon_intro'=>'Analyzr is completely open source with the flexibility of self hosting within your AWS account.',
+    'xervmon_intro'=>'Analyzr is completely open source with the flexibility of hosting using your AWS account.',
     'xervmon_call_to_action'=>'Learn how you can save upto 50% on cloud costs with Xervmon'
 );
